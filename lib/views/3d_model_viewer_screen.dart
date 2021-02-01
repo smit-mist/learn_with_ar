a32c615eaca868b6b954d6ba11a25d0a0434fdf2
smit-mist <smit3355mistry@gmail.com>
2021-02-01 14:29:52 +0530

    AR Core Tested a Sphere modal

--- lib/views/3d_model_viewer_screen.dart
+++ lib/views/3d_model_viewer_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ModalViewer extends StatefulWidget {
   @override
@@ -6,11 +8,37 @@ class ModalViewer extends StatefulWidget {
 }
 
 class _ModalViewerState extends State<ModalViewer> {
+  ArCoreController _controller;
+  _onArCoreView(ArCoreController controller) {
+    _controller = controller;
+    _addSphere(_controller);
+  }
+
+  _addSphere(ArCoreController _ar) {
+    final material = ArCoreMaterial(color: Colors.purple);
+    final sphere = ArCoreSphere(materials: [material], radius: 0.2);
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(
+        0,0,-1
+      ),
+    );
+    _controller.addArCoreNode(node);
+  }
+@override
+  void dispose() {
+    _controller.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Modal Viewer'),
+      ),
+      body: ArCoreView(
+        onArCoreViewCreated: _onArCoreView,
       ),
     );
   }
