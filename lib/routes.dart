281393335c63b739eee1a5181694fe1d2ff41fe5
Smit Rami <smitrami6661@gmail.com>
2021-02-02 23:51:52 +0530

    Dashboard grid view in progress

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/smit3/AndroidStudioProjects/learn_with_ar/lib/views/testing/3d_model_viewer_screen.dart';
+import 'views/testing/3d_model_viewer_screen.dart';
 import 'package:learn_with_ar/views/dashboard_screen.dart';
 import 'package:learn_with_ar/views/login_screen.dart';
-import 'file:///C:/Users/smit3/AndroidStudioProjects/learn_with_ar/lib/views/testing/obj_modal_view.dart';
-import 'file:///C:/Users/smit3/AndroidStudioProjects/learn_with_ar/lib/views/testing/render_object.dart';
+import 'views/testing/obj_modal_view.dart';
+import 'views/testing/render_object.dart';
 
 Map<String, Widget Function(BuildContext)> route_generator = {
   'login': (context) => LoginScreen(),
   'modal': (context) => ModalViewer(),
   'object': (context) => RenderTest(),
   'obj': (context) => ObjModalViewer(),
-  'dash':(context)=>DashboardScreen(),
+  'dash': (context) => DashboardScreen(),
 };
--- lib/views/dashboard_screen.dart
+++ lib/views/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -11,15 +12,79 @@ class _DashboardScreenState extends State<DashboardScreen> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hey This is Home Page,'),
-      ),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, 'object');
-        },
-      ),
-    );
+        appBar: AppBar(),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[100],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[200],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[300],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[400],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[500],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[600],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[600],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.teal[600],
+              ),
+              padding: const EdgeInsets.all(8),
+              child: const Text('Smit'),
+            ),
+          ],
+        ));
   }
 }
