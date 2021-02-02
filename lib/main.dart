59194048a8c8117be42e96d6b3d9957113d00c2b
smit-mist <smit3355mistry@gmail.com>
2021-02-02 15:17:11 +0530

    Login Screen Added UI Changes tobe done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:learn_with_ar/routes.dart';
 import 'package:learn_with_ar/views/login_screen.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_login/flutter_login.dart';
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
   runApp(MyApp());
 }
 
@@ -10,16 +17,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Learn with AR',
       routes: route_generator,
-      initialRoute: 'obj',
+      initialRoute: 'login',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.orange,
+        cursorColor: Colors.orange,
+        textTheme: TextTheme(
+          display2: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            color: Colors.orange,
+          ),
+          button: TextStyle(
+            fontFamily: 'OpenSans',
+          ),
+          caption: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          display4: TextStyle(fontFamily: 'Quicksand'),
+          display3: TextStyle(fontFamily: 'Quicksand'),
+          display1: TextStyle(fontFamily: 'Quicksand'),
+          headline: TextStyle(fontFamily: 'NotoSans'),
+          title: TextStyle(fontFamily: 'NotoSans'),
+          subhead: TextStyle(fontFamily: 'NotoSans'),
+          body2: TextStyle(fontFamily: 'NotoSans'),
+          body1: TextStyle(fontFamily: 'NotoSans'),
+          subtitle: TextStyle(fontFamily: 'NotoSans'),
+          overline: TextStyle(fontFamily: 'NotoSans'),
+        ),
       ),
       home: LoginScreen(),
     );
   }
 }
-
-
