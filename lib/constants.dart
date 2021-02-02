ece86325a489d53ed2897eabcfc6712910dc97cf
vaibhav720 <parikhvaibhav19@gmail.com>
2021-02-02 12:28:04 +0530

    login page

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String appName = 'Learn with AR';
+  static const String logoTag = 'near.huscarl.loginsample.logo';
+  static const String titleTag = 'near.huscarl.loginsample.title';
+}
