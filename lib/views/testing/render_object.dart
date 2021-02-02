3a1763d84f3b8b5f772897e3519c20f5cf76210c
smit-mist <smit3355mistry@gmail.com>
2021-02-02 22:54:26 +0530

    Pump Tested

--- lib/views/testing/render_object.dart
+++ lib/views/testing/render_object.dart
@@ -9,23 +9,42 @@ class RenderTest extends StatefulWidget {
 class _RenderTestState extends State<RenderTest> {
   //https://modelviewer.dev/shared-assets/models/Astronaut.glb - astro
   //https://firebasestorage.googleapis.com/v0/b/learn-with-ar.appspot.com/o/apollo.glb?alt=media&token=6ca3250f-9e91-4b7c-9bb2-4c0228585a2a
-  String url = 'https://firebasestorage.googleapis.com/v0/b/learn-with-ar.appspot.com/o/apollo.glb';
+  String url =
+      'https://firebasestorage.googleapis.com/v0/b/learn-with-ar.appspot.com/o/apollo.glb';
+  bool flag = false;
   @override
   Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Model Object",
         ),
       ),
-      body: ModelViewer(
-        src: url,
-        alt: "A 3D model of an astronaut",
-        ar: true,
-        arScale: "auto",
-        autoRotate: true,
-        autoRotateDelay: 0,
-        //   cameraControls: true,
+      body: Container(
+        height: h*0.8,
+        width: w,
+        child: ModelViewer(
+          src: 'assets/pump.glb',
+          alt: "A 3D model of an astronaut",
+          ar: true,
+          arScale: "auto",
+
+          autoRotate: flag,
+          autoRotateDelay: 0,
+          cameraControls: true,
+          //autoPlay: true,
+          //   cameraControls: true,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: (){
+          setState(() {
+            flag = !flag;
+          });
+        },
       ),
     );
   }
