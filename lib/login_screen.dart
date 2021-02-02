ea9e763b4c8cac472fb72c29058e4c1c4bbdf84a
vaibhav720 <parikhvaibhav19@gmail.com>
2021-02-02 12:28:13 +0530

    login page

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,201 +1,175 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:mun/logic/auth.dart';
-import 'package:mun/models/user.dart';
-import 'package:mun/views/elements/constants.dart';
-import 'package:mun/views/elements/textstyles.dart';
-import 'package:mun/views/elements/widgets.dart';
-import 'package:mun/views/elements/widgets/loading.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'constants.dart';
+import 'custom_route.dart';
+import 'dashboard_screen.dart';
+import 'users.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/auth';
 
-class _LoginScreenState extends State<LoginScreen> {
-  GlobalKey<FormState> _key = GlobalKey<FormState>();
-  bool isLoading = false;
-  bool isVisible = false;
-  final AuthService _auth = AuthService();
-  String email = "", password = "";
+  Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
 
-  void logMeIn() async {
-    if (_key.currentState.validate()) {
-      setState(() {
-        isLoading = true;
-      });
-      final User user = await _auth.signIn(email, password);
-      if (user != null) {
-        MUNUser currentUser = _auth.userFromFirebaseUser(user);
-        Navigator.pushNamedAndRemoveUntil(context, 'home', (route) => false);
-      } else {
-        setState(() {
-          isLoading = false;
-        });
-        Fluttertoast.showToast(
-          msg: 'Invalid email or password',
-          fontSize: 16.0,
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.BOTTOM,
-        );
+  Future<String> _loginUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(data.name)) {
+        return 'Username not exists';
       }
-    }
+      if (mockUsers[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(name)) {
+        return 'Username not exists';
+      }
+      return null;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    double h = MediaQuery.of(context).size.height;
-    double w = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: isLoading
-          ? Loading()
-          : Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Center(
-                      child: Text(
-                        'BookMyMun',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Helvetica',
-                          fontSize: 35,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: h * 0.1,
-                    ),
-                    SingleChildScrollView(
-                      child: Container(
-                        height: h * 0.15,
-                        child: Form(
-                          key: _key,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                width: w * 0.85,
-                                child: TextFormField(
-                                  validator: (value) {
-                                    return (value.isEmpty ||
-                                            !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                                .hasMatch(value))
-                                        ? 'Enter a valid email'
-                                        : null;
-                                  },
-                                  cursorColor: Colors.black,
-                                  onChanged: (change) {
-                                    setState(() {
-                                      email = change;
-                                    });
-                                  },
-                                  decoration: textFieldDecoration('Email'),
-                                ),
-                              ),
-                              Container(
-                                width: w * 0.85,
-                                child: TextFormField(
-                                  validator: (value) {
-                                    return (value.isEmpty || value.length < 8)
-                                        ? 'Minimum length of password is 8'
-                                        : null;
-                                  },
-                                  cursorColor: Colors.black,
-                                  onChanged: (change) {
-                                    setState(() {
-                                      password = change;
-                                    });
-                                  },
-                                  obscureText: isVisible ? false : true,
-                                  decoration: textFieldDecoration('Password'),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: (w * 0.075 - 12) < 0 ? 0 : (w * 0.075 - 12),
-                          right: (w * 0.075 - 5) < 0 ? 0 : (w * 0.075 - 5)),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Checkbox(
-                            value: isVisible,
-                            activeColor: kBlueShade,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                isVisible = newValue;
-                              });
-                            },
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 18),
-                            child: Text(
-                              'Show Password',
-                              style: simple(14),
-                            ),
-                          ),
-                          Spacer(),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(context, 'forgot_password');
-                            },
-                            child: Text(
-                              'Forgot password?',
-                              style: simple(14).copyWith(color: kBlueShade),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Center(
-                      child: NormalButton(
-                        text: 'Log In',
-                        function: logMeIn,
-                        width: w * 0.85,
-                        height: h * 0.05,
-                      ),
-                    ),
-                    SizedBox(
-                      height: h * 0.05,
-                    ),
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Don't have an account?",
-                            style: simple(18),
-                          ),
-                          TextButton(
-                            child: Text(
-                              "Sign Up",
-                              style: simple(18).copyWith(
-                                color: kBlueShade,
-                              ),
-                            ),
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .pushReplacementNamed('sign_up');
-                            },
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
+    final inputBorder = BorderRadius.vertical(
+      bottom: Radius.circular(10.0),
+      top: Radius.circular(20.0),
+    );
+
+    return FlutterLogin(
+      title: Constants.appName,
+      logo: 'assets/images/ecorp.png',
+      logoTag: Constants.logoTag,
+      titleTag: Constants.titleTag,
+      // messages: LoginMessages(
+      //   usernameHint: 'Username',
+      //   passwordHint: 'Pass',
+      //   confirmPasswordHint: 'Confirm',
+      //   loginButton: 'LOG IN',
+      //   signupButton: 'REGISTER',
+      //   forgotPasswordButton: 'Forgot huh?',
+      //   recoverPasswordButton: 'HELP ME',
+      //   goBackButton: 'GO BACK',
+      //   confirmPasswordError: 'Not match!',
+      //   recoverPasswordIntro: 'Don\'t feel bad. Happens all the time.',
+      //   recoverPasswordDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+      //   recoverPasswordSuccess: 'Password rescued successfully',
+      // ),
+      // theme: LoginTheme(
+      //   primaryColor: Colors.teal,
+      //   accentColor: Colors.yellow,
+      //   errorColor: Colors.deepOrange,
+      //   pageColorLight: Colors.indigo.shade300,
+      //   pageColorDark: Colors.indigo.shade500,
+      //   titleStyle: TextStyle(
+      //     color: Colors.greenAccent,
+      //     fontFamily: 'Quicksand',
+      //     letterSpacing: 4,
+      //   ),
+      //   // beforeHeroFontSize: 50,
+      //   // afterHeroFontSize: 20,
+      //   bodyStyle: TextStyle(
+      //     fontStyle: FontStyle.italic,
+      //     decoration: TextDecoration.underline,
+      //   ),
+      //   textFieldStyle: TextStyle(
+      //     color: Colors.orange,
+      //     shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
+      //   ),
+      //   buttonStyle: TextStyle(
+      //     fontWeight: FontWeight.w800,
+      //     color: Colors.yellow,
+      //   ),
+      //   cardTheme: CardTheme(
+      //     color: Colors.yellow.shade100,
+      //     elevation: 5,
+      //     margin: EdgeInsets.only(top: 15),
+      //     shape: ContinuousRectangleBorder(
+      //         borderRadius: BorderRadius.circular(100.0)),
+      //   ),
+      //   inputTheme: InputDecorationTheme(
+      //     filled: true,
+      //     fillColor: Colors.purple.withOpacity(.1),
+      //     contentPadding: EdgeInsets.zero,
+      //     errorStyle: TextStyle(
+      //       backgroundColor: Colors.orange,
+      //       color: Colors.white,
+      //     ),
+      //     labelStyle: TextStyle(fontSize: 12),
+      //     enabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     errorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade700, width: 7),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedErrorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade400, width: 8),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     disabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.grey, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //   ),
+      //   buttonTheme: LoginButtonTheme(
+      //     splashColor: Colors.purple,
+      //     backgroundColor: Colors.pinkAccent,
+      //     highlightColor: Colors.lightGreen,
+      //     elevation: 9.0,
+      //     highlightElevation: 6.0,
+      //     shape: BeveledRectangleBorder(
+      //       borderRadius: BorderRadius.circular(10),
+      //     ),
+      //     // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      //     // shape: CircleBorder(side: BorderSide(color: Colors.green)),
+      //     // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
+      //   ),
+      // ),
+      emailValidator: (value) {
+        if (!value.contains('@') || !value.endsWith('.com')) {
+          return "Email must contain '@' and end with '.com'";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value.isEmpty) {
+          return 'Password is empty';
+        }
+        return null;
+      },
+      onLogin: (loginData) {
+        print('Login info');
+        print('Name: ${loginData.name}');
+        print('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+      onSignup: (loginData) {
+        print('Signup info');
+        print('Name: ${loginData.name}');
+        print('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(FadePageRoute(
+          builder: (context) => DashboardScreen(),
+        ));
+      },
+      onRecoverPassword: (name) {
+        print('Recover password info');
+        print('Name: $name');
+        return _recoverPassword(name);
+        // Show new password dialog
+      },
+      showDebugButtons: true,
     );
   }
 }
