983914bdddc1e0a2b997d3ec7b0fe41d16f4d1ff
smit-mist <smit3355mistry@gmail.com>
2021-02-03 01:44:29 +0530

    StateFul Widget Created

--- lib/views/dashboard_screen.dart
+++ lib/views/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
+import 'package:learn_with_ar/views/widgets/dashboard_grid_container.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -14,95 +15,100 @@ class _DashboardScreenState extends State<DashboardScreen> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          Navigator.pushNamed(context, 'object');
-        }),
-        appBar: AppBar(
-          actions: [
-            GestureDetector(
-              child: Icon(
-                Icons.logout,
-              ),
-              onTap: () async {
-                await _auth.signOut();
-                Navigator.pushNamedAndRemoveUntil(
-                    context, 'login', (route) => false);
-              },
-            )
-          ],
-        ),
-        body: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[100],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Navigator.pushNamed(context, 'object');
+      }),
+      appBar: AppBar(
+        actions: [
+          GestureDetector(
+            child: Icon(
+              Icons.logout,
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[200],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+            onTap: () async {
+              await _auth.signOut();
+              Navigator.pushNamedAndRemoveUntil(
+                  context, 'login', (route) => false);
+            },
+          ),
+        ],
+      ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[100],
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[300],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[200],
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[400],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[300],
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[500],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[400],
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[600],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[500],
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[600],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[600],
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.teal[600],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Smit'),
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[600],
             ),
-          ],
-        ));
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.teal[600],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: const Text('Smit'),
+          ),
+          GridContainer(
+            color: Colors.redAccent,
+            text: 'Hello',
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/dashboard_grid_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+// Animated Container no video joi leje..
+// Widget of the week mathi..
+class GridContainer extends StatefulWidget {
+  Color color; // any color aa pachi nai joi pan atyare joi che so...
+  String text; // math, science etc..
+  String imageUrl; // aa pachi avse atyare nathi..
+  GridContainer({this.text, this.color, this.imageUrl}); // constructor che aa..
+  @override
+  _GridContainerState createState() => _GridContainerState();
+}
+
+class _GridContainerState extends State<GridContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration:
+          Duration(milliseconds: 100), // koi pan animation thai ano time...
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            20,
+          ),
+        ),
+        color: widget.color,
+      ),
+      padding: EdgeInsets.all(8),
+      child: Text(
+        '${widget.text}',
+      ),
+    );
+  }
+}
