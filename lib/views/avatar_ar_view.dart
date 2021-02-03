7120a8a69667e4105b0dc2414f0326c16ff05736
smit-mist <smit3355mistry@gmail.com>
2021-02-04 01:46:38 +0530

    Last Changes

--- lib/views/avatar_ar_view.dart
+++ lib/views/avatar_ar_view.dart
@@ -17,44 +17,45 @@ class _AvatarArViewState extends State<AvatarArView> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            widget.currentAvatar.name,
-          ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          widget.currentAvatar.name,
         ),
-        body: Stack(
-          children: [
-            AnimatedOpacity(
-              opacity: loading ? 1 : 0,
-              child: Container(
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              ),
-              duration: Duration(milliseconds: 200),
-            ),
-            AnimatedOpacity(
-              opacity: loading ? 0 : 1,
-              duration: Duration(milliseconds: 200),
-              child: ModelViewer(
-                enableColorChange: true,
-                backgroundColor: Colors.white,
-                src: widget.currentAvatar.modelUrl,
-                alt: widget.currentAvatar.description,
-                ar: true,
-                arScale: "auto",
-                autoRotate: true,
-                autoRotateDelay: 0,
-                cameraControls: true,
-                onModelIsVisisble: () {
-                  setState(() {
-                    loading = false;
-                  });
-                },
+      ),
+      body: Stack(
+        children: [
+          AnimatedOpacity(
+            opacity: loading ? 1 : 0,
+            child: Container(
+              child: Center(
+                child: CircularProgressIndicator(),
               ),
             ),
-          ],
-        ));
+            duration: Duration(milliseconds: 200),
+          ),
+          AnimatedOpacity(
+            opacity: loading ? 0 : 1,
+            duration: Duration(milliseconds: 200),
+            child: ModelViewer(
+              enableColorChange: true,
+              backgroundColor: Colors.white,
+              src: widget.currentAvatar.modelUrl,
+              alt: widget.currentAvatar.description,
+              ar: true,
+              arScale: "auto",
+              autoRotate: true,
+              autoRotateDelay: 0,
+              cameraControls: true,
+              onModelIsVisisble: () {
+                setState(() {
+                  loading = false;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
